b3c37bd4ae0c90bb06fc1913549bb0ea156c5e2c
Aeyongddi <zsn1527@gmail.com>
2024-04-18 11:46:53 +0900

    0418

---
+++ backweb2/src/main/java/backweb2/a01_begin/A01_HelloJava.java
@@ -0,0 +1,11 @@
+package backweb2.a01_begin;
+
+public class A01_HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 기본적인 자바 출력 구문: 마지막 ;(세미콜론)으로 명령종료
+		System.out.println("자바 시작");
+	}
+
+}

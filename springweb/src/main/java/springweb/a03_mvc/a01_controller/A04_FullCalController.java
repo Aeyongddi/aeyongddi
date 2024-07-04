629d1d96f6e6d9fe000d8e4e4647fcbc22419897
Aeyongddi <zsn1527@gmail.com>
2024-07-04 17:52:30 +0900

    0704

---
+++ springweb/src/main/java/springweb/a03_mvc/a01_controller/A04_FullCalController.java
@@ -0,0 +1,8 @@
+package springweb.a03_mvc.a01_controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class A04_FullCalController {
+
+}
---
+++ springweb/src/main/java/springweb/a03_mvc/a03_dao/A03_DeptDao.java
@@ -0,0 +1,10 @@
+package springweb.a03_mvc.a03_dao;
+
+import java.util.List;
+
+import springweb.z01_vo.Dept;
+
+public interface A03_DeptDao {
+	List<Dept> getDeptList(Dept sch);
+	
+}

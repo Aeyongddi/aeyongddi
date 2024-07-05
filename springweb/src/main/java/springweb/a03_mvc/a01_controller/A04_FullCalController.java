4aff78a2b80d915bfb8de7b98166a060d7fb69a1
Aeyongddi <zsn1527@gmail.com>
2024-07-05 17:37:09 +0900

    0705

--- springweb/src/main/java/springweb/a03_mvc/a01_controller/A04_FullCalController.java
+++ springweb/src/main/java/springweb/a03_mvc/a01_controller/A04_FullCalController.java
@@ -1,8 +1,13 @@
 package springweb.a03_mvc.a01_controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+// http://localhost:7080/springweb/calender.do
 public class A04_FullCalController {
-
+	@GetMapping("calender.do")
+	public String calendar() {
+		return "WEB-INF\\views\\a03_mvc\\a07_fullcalendar.jsp";
+	}
 }

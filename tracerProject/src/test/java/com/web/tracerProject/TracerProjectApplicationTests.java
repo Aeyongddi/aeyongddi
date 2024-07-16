996f91d389cec79e13637976da47d075d4d3f032
Aeyongddi <zsn1527@gmail.com>
2024-07-16 18:31:32 +0900

    0716 j

---
+++ tracerProject/src/test/java/com/web/tracerProject/TracerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.tracerProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

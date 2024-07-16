6c8ef95ef4d8ac50b4205a6594c5b84931379be1
aeyongddi <zsn1527@gmail.com>
2024-07-17 00:56:42 +0900

    0717 j

--- tracerProject/src/main/java/com/web/tracerProject/controller/Test_con.java
+++ tracerProject/src/main/java/com/web/tracerProject/controller/Test_con.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class Test {
+public class Test_con {
 	// http://localhost:5656/testOpen
 	@RequestMapping("/testOpen")
 	private String testOpen() {

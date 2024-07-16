fe386dbe7f23541fae5803744d07b8fa08a07b67
Aeyongddi <zsn1527@gmail.com>
2024-07-16 16:59:54 +0900

    0716

---
+++ final/src/main/java/vo/Test.java
@@ -0,0 +1,12 @@
+package vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Test {
+	private String aaa;
+}

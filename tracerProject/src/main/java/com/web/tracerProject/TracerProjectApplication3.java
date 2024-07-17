543fa889170365f88b892c9f9196a5390c79a1d6
human-16 <human-16@DESKTOP-KEVU0AD>
2024-07-17 17:02:34 +0900

    1023

---
+++ tracerProject/src/main/java/com/web/tracerProject/TracerProjectApplication3.java
@@ -0,0 +1,13 @@
+package com.web.tracerProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracerProjectApplication3 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracerProjectApplication3.class, args);
+	}
+
+}

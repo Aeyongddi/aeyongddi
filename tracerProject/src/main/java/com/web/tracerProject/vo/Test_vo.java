591eb3e9df9dd2b4613998483ae5ea5aa9e7bc4a
human-16 <human-16@DESKTOP-KEVU0AD>
2024-07-17 16:56:58 +0900

    nw

--- tracerProject/src/main/java/com/web/tracerProject/vo/Test_vo.java
+++ tracerProject/src/main/java/com/web/tracerProject/vo/Test_vo.java
@@ -1,5 +1,12 @@
 package com.web.tracerProject.vo;
 
-public class Test_vo {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class Test_vo {
+	private String aa;
 }

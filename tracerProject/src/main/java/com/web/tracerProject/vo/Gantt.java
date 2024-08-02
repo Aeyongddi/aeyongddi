a9e904dfc0d3200a80065bc06fd612634cbc70fc
human-16 <human-16@DESKTOP-KEVU0AD>
2024-08-02 11:09:28 +0900

    0802 N

---
+++ tracerProject/src/main/java/com/web/tracerProject/vo/Gantt.java
@@ -0,0 +1,28 @@
+package com.web.tracerProject.vo;
+
+import java.util.List;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Gantt {
+	private String id;
+	@DateTimeFormat(pattern = "yyyy-MM-dd hh:mm:ss")
+	private String start_date;
+	private String duration;
+	private String progress;
+	private String sortorder;
+	private String parent;
+	private String open;
+	private String text;
+	private String users;
+	private List<String> userList;
+}
